2e831a749eeced5b5f1903fa77a0b1b7a30aaaf6
Tapan <kumar.tapan2703@gmail.com>
2022-07-05 12:07:31 +0530

    Fixed nodemailern

--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 const connect = () => {
     console.log("MONGODB CONNECTED");
     const connectionString = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.iun3s.mongodb.net/?retryWrites=true&w=majority`
-
 return mongoose.connect(connectionString);
 }
 
